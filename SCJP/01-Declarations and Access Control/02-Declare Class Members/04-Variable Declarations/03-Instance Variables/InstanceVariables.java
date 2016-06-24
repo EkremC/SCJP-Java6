70080d8ed77642918ed634348880a2ec214abdd9
Ekrem, Candemir <ekremccandemir@gmail.com>
2016-06-25 01:22:38 +0300

    Instance Variables

---
+++ SCJP/01-Declarations and Access Control/02-Declare Class Members/04-Variable Declarations/03-Instance Variables/InstanceVariables.java
@@ -0,0 +1,23 @@
+
+//Instance variablelar, sınıf icerisinde tanımlanırlar ama methodların dısında olmalıdır.
+
+//Yalnızca sınıf instantiated oldugunda, initialize olurlar.
+
+//Bu degiskenler, her objenin kendisine ait olan degiskenlerdir.
+
+//Asagıdaki sınıfın, her bir objesinin kendisine ait, ismi, unvanı ve sayısı vardır.
+
+//4 access level'ı da kullanabilirler.(public, protected, private)
+
+//final ve transient olabilirler.
+
+//abstract, synchronized, strictfp, native olamazlar.
+
+//static de olamazlar. Cünkü static olan variablelar, sınıf degiskeni olurlar.
+
+public class InstanceVariables {
+
+	private String name;
+	public String title;
+	protected int number;
+}
