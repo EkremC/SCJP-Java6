574514ce6ed4e4fae60aeabfe96778e01d3dcab9
Ekrem, Candemir <ekremccandemir@gmail.com>
2016-06-25 00:57:18 +0300

    Declaring Reference Variables

--- SCJP/01-Declarations and Access Control/02-Declare Class Members/04-Variable Declarations/02-Declaring Reference Variables/DeclaringReferenceVariables.java
+++ SCJP/01-Declarations and Access Control/02-Declare Class Members/04-Variable Declarations/02-Declaring Reference Variables/DeclaringReferenceVariables.java
@@ -1,4 +1,10 @@
 
+//Reference degiskenler, sınıf degiskeni(static degisken), instance variable, method parametresi ve 
+//local(yerel) degiskenler olarak tanımlanabilirler.
+
 public class DeclaringReferenceVariables {
 
+	Object o;
+	Dog myNewDogReferenceVariable;
+	String s1, s2, s3;  //declare three String vars.
 }
