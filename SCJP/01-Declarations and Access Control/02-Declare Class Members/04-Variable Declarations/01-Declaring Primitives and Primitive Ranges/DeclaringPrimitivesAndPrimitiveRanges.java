2daf49ac934772d6d4767fd2509ef24aaf1cc15d
Ekrem, Candemir <ekremccandemir@gmail.com>
2016-06-25 00:43:09 +0300

    Declaring Primitives and Primitive Ranges

---
+++ SCJP/01-Declarations and Access Control/02-Declare Class Members/04-Variable Declarations/01-Declaring Primitives and Primitive Ranges/DeclaringPrimitivesAndPrimitiveRanges.java
@@ -0,0 +1,18 @@
+
+//8 tane primitive tip vardır; char, boolean, short, int, long, double, float
+
+//Primitive degiskenler, sınıf degiskeni(static degisken), instance variable, method parametresi ve local(yerel)
+//degiskenler olarak tanımlanabilirler.
+
+//char ve boolean haric, geri kalanlar signed(isaretli)dir. Bunun anlamı ise; negatif ya da pozitif olabilir.
+
+
+
+
+public class DeclaringPrimitivesAndPrimitiveRanges {
+	
+	byte b;
+	boolean myBooleanPrimitive;
+	int x, y, z;
+
+}
